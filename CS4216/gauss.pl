593296e45ce730e0d2e5a41bc085b9f84ea3b563
Shawn Tan <shawn@wtf.sg>
2012-02-07 18:47:19 +0800

    Normalise everything first!
    Seems to be working fine.

--- CS4216/gauss.pl
+++ CS4216/gauss.pl
@@ -1,51 +1,69 @@
-gaussian([]).
-gaussian(Eqns):-
-	normalise(Eqns,NEqn,Vars),
-	write(NEqn),write(Vars),nl,
-	gaussian_pass(Vars,NEqn),
-	solve_eqs(NEqn).
-
-gaussian_pass([],[]).
-gaussian_pass([Var],[N*Var = Val]) :- Var is Val/N,!.
-gaussian_pass([Var|RestVar],Eqns) :-
-	delete(Eqn,Eqns,RestEqns),
-	write([Var,Eqn]),nl,
-	eliminate_all(Var,RestVar,Eqn,RestEqns,Result),
-	gaussian_pass(RestVar,Result).
+lin_eq(Eqns) :-
+	normalise(Eqns,NEqns,Vars),
+	norm(Vars,NEqns,NNEqns),!,
+	gaussian(Vars,NNEqns).
 
 
-eliminate_all(_,_,_,[],[]).
-eliminate_all(Var,RestVar,Expr1=Val1,[Expr2=Val2|Rest],[Expr2El=ValEl|Result]) :-
-	eliminate(Var,RestVar,Expr1,Expr2,Expr2El),
-	eliminate_all(Var,RestVar,Expr1=Val1,Rest,Result),
-	get_coeff(Var,Expr1,A),
-	get_coeff(Var,Expr2,B),
-	ValEl is Val2/B - Val1/A.
-eliminate_all(Var,RestVar,Eqn,[CantEqn|Rest],[CantEqn|Result]) :-
-	eliminate_all(Var,RestVar,Eqn,Rest,Result).
-eliminate(_,[],_,_,0).
-eliminate(Var,[Var1|Rest],Expr1,Expr2,NewExpr) :-
-	eliminate(Var,Rest,Expr1,Expr2,Rest1),
-	get_coeff(Var,Expr1,A),
-	(get_coeff(Var1,Expr1,B);B is 0),
-	get_coeff(Var,Expr2,C),
-	(get_coeff(Var1,Expr2,D); D is 0),
-	N is  D/C-B/A,
-	(Rest1 == 0 -> NewExpr = N*Var1;NewExpr = Rest1 + N*Var1).	
+gaussian([_],[Expr]) :- solve_x(Expr).
+gaussian([Var|Vars],Eqns):-
+	delete(Expr = Val,Eqns,RestEqns), get_coeff(Var,Expr,N),N\=0,!,
+	eliminate_all(Var,Vars,Expr=Val,RestEqns,ResEqns),
+	gaussian(Vars,ResEqns),
+	solve_x(Expr=Val).
 
-solve_eqs([]).
-solve_eqs([Eqn|Rest]) :-
-	solve_eqs(Rest),
-	solve_eq(Eqn,N*X = M),
-	(ground(X);X is M/N).
-solve_eq(Expr + N*X = Val,Expr = NewVal) :- 
-	ground(X),
-	NewVal is Val-(N*X),!.
-solve_eq(A=B, A=B).
+solve_x(N*X = Val):-X is Val/N.
+solve_x(M + N*X = Val) :-ground(M),M1 is M,X is (Val-M1)/N.
 
+eliminate_all(Var,Vars,Expr1 = Val1,[Expr2 = Val2],[NewExpr = NewVal]) :-
+	pairwise(Var,Vars,Expr1,Expr2,NewExpr),
+	cancel_terms(Var,Expr1 = Val1,Expr2 = Val2,NewVal).
+eliminate_all(Var,Vars,Expr1 = Val1,[Expr2 = Val2|RestEqn],[NewExpr = NewVal|NewRestEqn]) :-
+	pairwise(Var,Vars,Expr1,Expr2,NewExpr),
+	eliminate_all(Var,Vars,Expr1 = Val1,RestEqn,NewRestEqn),
+	cancel_terms(Var,Expr1 = Val1,Expr2 = Val2,NewVal).
+
+
+pairwise(Var,[V1],Expr1,Expr2,N*V1):-
+	cancel_terms(Var,V1,Expr1,Expr2,N).
+pairwise(Var,[V1|Vars],Expr1,Expr2,NewExpr+N*V1) :-
+	pairwise(Var,Vars,Expr1,Expr2,NewExpr),
+	cancel_terms(Var,V1,Expr1,Expr2,N).
+
+
+cancel_terms(Var,V1,Expr1,Expr2,N) :-
+	get_coeff(Var,Expr2,A2),
+	get_coeff(V1,Expr2,N2),
+	(A2 \= 0 -> 
+		(get_coeff(Var,Expr1,A1),
+		get_coeff(V1,Expr1,N1),
+		Norm1 is N1/A1,
+		Norm2 is N2/A2,
+		N is Norm2 - Norm1);
+		N is N2
+	).
+	
+cancel_terms(Var,Expr1 = Val1,Expr2 = Val2,N) :-
+	get_coeff(Var,Expr2,A2),
+	(A2 \= 0 ->
+		(get_coeff(Var,Expr1,A1),N is Val2/A2 - Val1/A1);
+		N is Val2
+	).
+
+
+norm(_,[],[]):-!.
+norm(Vars,[Expr = Val|Rest],[NewExpr = Val|NewRest]) :-
+	norm(Vars,Rest,NewRest),
+	rebuild_eq(Vars,Expr,NewExpr).
+
+rebuild_eq([Var],Expr,N*Var) :- get_coeff(Var,Expr,N).
+rebuild_eq([Var|Rest],Expr,NewExpr + N*Var) :-
+	get_coeff(Var,Expr,N),
+	rebuild_eq(Rest,Expr,NewExpr).
+	
 get_coeff(Var1,N*Var2,N) :- (Var1 == Var2),!.
 get_coeff(Var1,_ + N*Var2,N) :- (Var1 == Var2),!.
-get_coeff(Var,Rest + _,N) :- get_coeff(Var,Rest,N).
+get_coeff(Var,Rest + _,N) :- get_coeff(Var,Rest,N),!.
+get_coeff(_,_,0).
 
 normalise([],[],[]).
 normalise([Expr = N |Rest],[NExpr = N|NRest],Uniqs) :-
@@ -54,14 +72,13 @@ normalise([Expr = N |Rest],[NExpr = N|NRest],Uniqs) :-
 	append(URest,Uniq,Uniqs1),
 	sort(Uniqs1,Uniqs).
 
-
-
-normalise_eq(Expr - Y,Expr1 + (-N*Y1),[Y1|L]) :- 	normalise_eq(Expr,Expr1,L),normalise_coeff(Y,N,Y1).
-normalise_eq(Expr + Y,Expr1 + N*Y1,[Y1|L]) :- 		normalise_eq(Expr,Expr1,L),normalise_coeff(Y,N,Y1).
 normalise_eq(Y,N*Y1,[Y1]) :- normalise_coeff(Y,N,Y1).
+normalise_eq(Expr - Y,Expr1 + K*Y1,[Y1|L]) :- 	normalise_eq(Expr,Expr1,L),normalise_coeff(Y,N,Y1), K is -N.
+normalise_eq(Expr + Y,Expr1 + N*Y1,[Y1|L]) :- 		normalise_eq(Expr,Expr1,L),normalise_coeff(Y,N,Y1).
+normalise_coeff(X,1,X) :- var(X).
+normalise_coeff(-X,-1,X) :- var(X).
 normalise_coeff(X*N,N,X) :- number(N).
 normalise_coeff(N*X,N,X) :- number(N).
-normalise_coeff(X,1,X).
 
 %:-eliminate(X,[Y,Z],1*X + 2*Y + 2*Z, 2*X + 4*Y + 2*Z,G),nl,write(G),nl.
 %:-eliminate_all(X,[Y,Z],1*X + 2*Y + 2*Z = 5, [2*X + 4*Y + 2*Z=2],G),nl,write(G),nl.
@@ -75,11 +92,17 @@ normalise_coeff(X,1,X).
 % nl,
 % write(System1),nl.
 
-
- :- System = 
-	[2*X + 3*Y = 6,
-	 3*X + 3*Y = 9],
- 	write(System),nl,
-	gaussian(System),
-	write(System),nl,
-	write(X),nl,write(Y),nl. 
+:- lin_eq([
+		X + 2*Y + 3*Z = 8,
+		  Y  + X   = 3,
+		X - 3*Y + 5*Z = 0]),
+write(X),nl,
+write(Y),nl,
+write(Z),nl.
+% :- System = 
+%	[3*Y = 6,
+%	 3*Y + 3*X = 9],
+% 	write(System),nl,
+%	gaussian(System),
+%	write(System),nl,
+%	write(X),nl,write(Y),nl. 
