310378cc083651eebbe65fc570041178416fa15e
Shawn Tan <shawn@wtf.sg>
2012-02-14 21:47:48 +0800

    Damn.

--- CS4216/PS2/ex2.pl
+++ CS4216/PS2/ex2.pl
@@ -1,5 +1,13 @@
-:- dynamic qs/2.
+:- dynamic p/1.
+:- dynamic q/1.
+:- dynamic r/1.
+r(X) :- q(X).
+q(X) :- p(X).
+p(t).
 
+:- dynamic qs/2.
+:- dynamic part/4.
+:- dynamic app/3.
 qs([],[]).
 qs([X|Xs],Ys) :-
 	part(X,Xs,Littles,Bigs),
@@ -10,8 +18,22 @@ part(_,[],[],[]).
 part(X,[Y|Xs],[Y|Ls],Bs) :- X > Y, part(X,Xs,Ls,Bs).
 part(X,[Y|Xs],Ls,[Y|Bs]) :- X =< Y, part(X,Xs,Ls,Bs).
 
-solve( true, 0 ) :- !.
-solve( (A,B), N ) :- !,solve(A,N1), solve(B,N2), N is N1 + N2.
-solve( H, N ) :- clause(H,Body),solve(Body,M), N is M+1.
+app([],Ys,Ys).
+app([X|Xs],Ys,[X|Zs]) :- app(Xs,Ys,Zs).
 
 
+
+solve(true) :- !.
+solve(A) :- arithmetic(A),incval(step_count), !,A.
+solve((A,B)) :- !,solve(A), solve(B).
+solve(H) :-
+	clause(H,Body),
+	incval(step_count),
+	solve(Body).
+
+arithmetic(_<_).
+arithmetic(_>_).
+arithmetic(_=<_).
+arithmetic(_>=_).
+arithmetic(_=:=_).
+arithmetic(_=\=_).
--- CS4216/PS2/meta.pl
+++ CS4216/PS2/meta.pl
@@ -1,18 +1,31 @@
-:- dynamic p/1.
-:- dynamic q/1.
-:- dynamic r/1.
-
-p(X) :- q(X).
-q(X) :- r(X).
-r(X) :- true.
-
 solve(true,_) :- !.
 solve((A,B),Callstack) :- !,
 	solve(A,Callstack),
 	solve(B,Callstack).
 solve(H,Callstack) :-!,
 	clause(H,Body),
-	(member(H,Callstack) ->
+	(strict_member(H,Callstack) ->
 		write('Infinite loop detected, execution aborted'),nl;
 		solve(Body,[H|Callstack])
 	).
+
+
+arithmetic(_<_).
+arithmetic(_>_).
+arithmetic(_=<_).
+arithmetic(_>=_).
+arithmetic(_=:=_).
+arithmetic(_=\=_).
+
+strict_member(H,[H1]) :- H==H1.
+strict_member(H,[H1|_]) :- H==H1,!.
+strict_member(H,[_|T]) :- strict_member(H,T).
+
+:- dynamic p/2.
+:- dynamic q/1.
+:- dynamic r/1.
+
+p(X,[X]).
+p(X,[H|T]) :- p(X,T).
+
+
