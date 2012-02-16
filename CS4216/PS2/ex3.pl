b01a76a3416d3e0c199a82cc81a51f0560a42cee
Shawn Tan <shawn@wtf.sg>
2012-02-17 01:38:14 +0800

    Again!!

--- CS4216/PS2/ex3.pl
+++ CS4216/PS2/ex3.pl
@@ -1,16 +1,24 @@
 
 
 all_perms(A) :- 
+	Variables = [X,Y,Z],
 	Domain = [
 		[1,2,3,4,5],
 		[4,5,6,7],
 		[0,1,2,3,4,5,6]
 	],
+	Constraints = [
+		X < Y,
+		Y < Z
+	],
 	length(Domain,N),length(A,N),
 	(
-		fromto(	(Domain,A),
-	   			([D|RestD],[E|RestA]),
-				(RestD,RestA),
-			   	([],[])	
-		) do member(E,D)
+		fromto(	(Variables,Domain,A),
+	   			([X|RestV],[D|RestD],[E|RestA]),
+				(RestV,RestD,RestA),
+			   	([],[],[])	
+		) do member(E,D), X is E
+	),
+	(
+		foreach(Con,Constraints) do Con
 	).
