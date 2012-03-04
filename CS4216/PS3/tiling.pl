a38900bba94ff0701d790b21ef5eb45d4774d424
Shawn Tan <shawn@wtf.sg>
2012-03-04 21:28:52 +0800

    Stupid method that doesn't seem to work.

--- CS4216/PS3/tiling.pl
+++ CS4216/PS3/tiling.pl
@@ -10,28 +10,27 @@ constraints(N,Missing,Grid,VarList):-
 	CellCount is (N*N)-M,
 	T is CellCount/2,
 	integer(T,TileCount),
-	length(VarList,CellCount),
+	VarList :: [0..TileCount],
 	(
-		multifor([I,J],1,N),param(Grid,N,Missing,VarList) do
-				(member(I-J,Missing) ->
-					subscript(Grid,[I,J],x);
+		multifor([I,J],1,N),param(Grid,N,Missing),foreach(Var,VarList) do
+			(member((I-J),Missing) ->
+				subscript(Grid,[I,J],x),Var=0;
+				(
+					subscript(Grid,[I,J],C),
+					Var = C,
+					(J > 1 -> subscript(Grid,[I,J-1],Up);	Up		= edge),
+					(J < N -> subscript(Grid,[I,J+1],Down);	Down	= edge),
+					(I > 1 -> subscript(Grid,[I-1,J],Left);	Left	= edge),
+					(I < N -> subscript(Grid,[I+1,J],Right);Right	= edge),
 					(
-						subscript(Grid,[I,J],C),
-						member(C,VarList),
-						(J > 1 -> subscript(Grid,[I,J-1],Up);Up = edge),
-						(J < N -> subscript(Grid,[I,J+1],Down);Down = edge),
-						(I > 1 -> subscript(Grid,[I-1,J],Left);Left = edge),
-						(I < N -> subscript(Grid,[I+1,J],Right);Right = edge),
-						(
-							C $=  Up and C $\= Left and C $\= Down and C $\= Right or
-							C $\= Up and C $=  Left and C $\= Down and C $\= Right or
-							C $\= Up and C $\= Left and C $=  Down and C $\= Right or
-							C $\= Up and C $\= Left and C $\= Down and C $=  Right
-						)
+						(C $=  Up and C $\= Left and C $\= Down and C $\= Right) or
+						(C $\= Up and C $=  Left and C $\= Down and C $\= Right) or
+						(C $\= Up and C $\= Left and C $=  Down and C $\= Right) or
+						(C $\= Up and C $\= Left and C $\= Down and C $=  Right)
 					)
 				)
-	),
-	distinct(TileCount,VarList).
+			)
+	).
 
 memberlist([],_).
 memberlist([H|T],L) :- member(H,L), memberlist(T,L).
@@ -42,6 +41,5 @@ sorted([H1,H2|T]) :- H1 $< H2, sorted([H2|T]).
 
 distinct(K,L) :-
    length(M,K),
-   write(M),nl,
    sorted(M), % could be replaced by all_distinct(M), leads to more solutions.
    memberlist(M,L), memberlist(L,M).
