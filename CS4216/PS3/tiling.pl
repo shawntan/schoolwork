092fd9a9f733cf33d1b2bcf93adb6a096f3eb038
Shawn Tan <shawn@wtf.sg>
2012-03-05 01:16:46 +0800

    Okay...

--- CS4216/PS3/tiling.pl
+++ CS4216/PS3/tiling.pl
@@ -11,28 +11,29 @@ constraints(N,Missing,TileCount,Grid,VarList):-
 	CellCount is (N*N)-M,
 	T is CellCount/2,
 	integer(T,TileCount),
+	length(VarList,CellCount),
 	(
-		multifor([I,J],1,N),param(Grid,N,Missing),foreach(Var,VarList) do
+		multifor([I,J],1,N),param(Grid,Missing,N,VarList),fromto(VarList,InVars,OutVars,[]),param(VarList) do
+		(
 			(member((I-J),Missing) ->
-				subscript(Grid,[I,J],0),Var=0;
+				OutVars = InVars,
+				subscript(Grid,[I,J],0);
 				(
 					subscript(Grid,[I,J],C),
-					Var = C,
-					(J > 1 -> subscript(Grid,[I,J-1],Up);	Up		= 0),
-					(J < N -> subscript(Grid,[I,J+1],Down);	Down	= 0),
-					(I > 1 -> subscript(Grid,[I-1,J],Left);	Left	= 0),
-					(I < N -> subscript(Grid,[I+1,J],Right);Right	= 0),
-					(
-						(C $=  Up and C $\= Left and C $\= Down and C $\= Right) or
-						(C $\= Up and C $=  Left and C $\= Down and C $\= Right) or
-						(C $\= Up and C $\= Left and C $=  Down and C $\= Right) or
-						(C $\= Up and C $\= Left and C $\= Down and C $=  Right)
-					)
+					InVars = [C|OutVars],
+					(C $= L or C $= R or C $= U or C $= D),
+					(I>1 -> subscript(Grid,[I-1,J],L);L is -1),
+					(I<N -> subscript(Grid,[I+1,J],R);R is -2),
+					(J>1 -> subscript(Grid,[I,J-1],U);U is -3),
+					(J<N -> subscript(Grid,[I,J+1],D);D is -4),
+					L $\= R,L $\= U,L $\= D,
+					R $\= U,R $\= D,
+					U $\= D
 				)
 			)
+		)
 	),
-	TC is TileCount+1,
-	distinct(TC,VarList).
+	distinct(TileCount,VarList).
 
 search(TileCount,VarList) :-
 	(
@@ -50,16 +51,17 @@ select_val(1,N,Col) :-
 	),Q.
 
 memberlist([],_).
-memberlist([H|T],L) :- member(H,L), memberlist(T,L).
+memberlist([H|T],L) :- suspend(member(H,L)), memberlist(T,L).
 
 sorted([]).
 sorted([_]).
 sorted([H1,H2|T]) :- H1 $< H2, sorted([H2|T]).
 
 distinct(K,L) :-
-   length(M,K),
-   sorted(M), % could be replaced by all_distinct(M), leads to more solutions.
-   memberlist(M,L), memberlist(L,M).
+	length(M,K),
+	sorted(M), % could be replaced by all_distinct(M), leads to more solutions.
+	memberlist(M,L),
+	memberlist(L,M).
 
 %:-constraints(3,[2-2],G,V),
 %	nl,nl,
