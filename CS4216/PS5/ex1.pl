7eb7c3669b9455a5df1a5d4be1fdca8393edee8d
Shawn Tan <shawn@wtf.sg>
2012-03-21 18:17:13 +0800

    Jerk-off current rule.

--- CS4216/PS5/ex1.pl
+++ CS4216/PS5/ex1.pl
@@ -1,16 +1,39 @@
-constraints(Circuit,[A|T],Cons) :-
-	delete(ground(A),Circuit,Remaining),
-	loops(A,Remaining,[A|T],Cons).
 
-loops(Start,Circuit,[H|T],[Con|ConT]) :-
-	delete(Comp,Circuit,Remaining),
-	Comp =.. [P,H,N,Val],
-	comp_cons(P,H,N,Val,Con),
-	(N == Start ->
-		T=[];
-		T=[N|T1],loops(Start,Remaining,[N|T1],ConT)
+:-lib(eplex).
+
+/*
+ * Kirchoff's current rule
+ */
+current(Circuit,AugCircuit,Points) :-
+	(
+		foreach(C,Circuit),
+		foreach((C,_),AugCircuit),
+		fromto([],PIn,POut,PointsDup)
+		do
+			(C =.. [_,A,B,_] ->
+				POut = [A,B|PIn];
+				POut = PIn
+			)
+	),
+	sort(PointsDup,Points).
+current_cons(Points,AugCircuit) :-
+	(
+		foreach(P,Points),param(AugCircuit) do
+		point_con(P,AugCircuit,Currs),
+		sum(Currs) $= 0
+	).
+point_con(Point,AugCircuit,Currs) :-
+	(
+		foreach((C,I),AugCircuit),
+		fromto([],CurIn,CurOut,Currs),param(Point) do
+		((C =.. [_,Point,_,_],Cur = -I ; C =.. [_,_,Point,_],Cur = I) ->
+			CurOut = [Cur|CurIn];
+			CurOut = CurIn
+		)
 	).
 
-comp_cons(battery,H,N,Volts,Volts).
-comp_cons(resistor,H,N,Ohms,I*Ohms).
+test(Circuit,AugCircuit,Point):-
+	Circuit = [ground(a),battery(a,b,10),resistor(b,a,1),resistor(b,a,1)],
+	current(Circuit,AugCircuit,Points),
+	current_cons(Points,AugCircuit).
 
