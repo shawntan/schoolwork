c4cc4c49184aced0ea54a0202a397e3c70c7d0d9
Shawn Tan <shawn@wtf.sg>
2012-03-22 15:17:02 +0800

    Ex1 should be done??

--- CS4216/PS5/ex1.pl
+++ CS4216/PS5/ex1.pl
@@ -36,24 +36,24 @@ point_con(Point,AugCircuit,Currs) :-
 /*
  *  Kirchoff's voltage rule
  */
-loop((C,I),RestCircuit,[Val|Loop]) :-
+loop((C,_),RestCircuit,[Val|Loop]) :-
 	C =.. [battery,A,B,Val],
 	(
 		fromto(
-			(B,  RestCircuit,Loop		),
-			(P1, InCirc,	[H|OutLoop]	),
-			(P2, OutCirc,	OutLoop		),
-			(A,  _,			[]			)
+			(B,  RestCircuit, Loop       ),
+			(P1, InCirc,      [H|OutLoop]),
+			(P2, OutCirc,     OutLoop    ),
+			(A,  _,           []         )
 		) do
-		delete((Seg,I),InCirc,OutCirc),
-		((
-			Seg =.. [C,P1,P2,V],
-			comp_volt(C,V,I,H)
-		);(
-			Seg =.. [C,P2,P1,V],
-			comp_volt(C,V,I,H1),
-			H = -H1
-		))
+			delete((Seg,I),InCirc,OutCirc),
+			((
+				Seg =.. [C,P1,P2,V],
+				comp_volt(C,V,I,H)
+			);(
+				Seg =.. [C,P2,P1,V],
+				comp_volt(C,V,I,H1),
+				H = -H1
+			))
 	).
 
 comp_volt(resistor,Val,I,-I*Val).
@@ -64,37 +64,53 @@ voltage_cons(AugCircuit,VoltList) :-
 		fromto([],ConsIn,ConsOut,VoltList),
 		param(AugCircuit) do
 			(C = (T,_), T =.. [battery|_] ->
-				writeln("hey!"),
 				delete(C,AugCircuit,Circuit),
 				bagof(Loop,loop(C,Circuit,Loop),Cons),
 				append(ConsIn,Cons,ConsOut);
 				ConsIn = ConsOut
 			)
 	).
-
-test_looper(Circuit,Cons,I1,I2,I3,I4,I5) :-
-	Circuit = [
-		(resistor(c,b,2),I1),
-		(resistor(c,d,2),I2),
-		(resistor(d,a,2),I3),
-		(resistor(d,a,2),I4),
-		(resistor(d,a,3),I5)
-	],
-	bagof(Loop,loop((battery(a,b,1),II),Circuit,Loop),Cons),
+compute_voltages(Done,_,Done,Points) :-
+	length(Done,N),length(Points,N),!.
+compute_voltages(Done,AugCircuit,Voltages,Points) :-
+	member((Comp,I),AugCircuit),
 	(
-		foreach(C,Cons) do
-		sum(C) $= 0
-	).
+		Comp =.. [C,A,B,Val],Dir = 1;
+		Comp =.. [C,B,A,Val],Dir = -1
+	),
+	not member((B,_),Done),
+	member((A,Vpre),Done),!,
+	comp_volt(C,Val,I,CompVolt),
+	eval(Vpre + Dir*CompVolt,Pot),
+	compute_voltages([(B,Pot)|Done],AugCircuit,Voltages,Points).
 
-test(Circuit,AugCircuit,Point):-
+solve(Circuit,Voltages) :-
+	delete(ground(A),Circuit,Circuit1), %assumes only 1.
+	preprocess(Circuit1,AugCircuit,Points),
+	current_cons(Points,AugCircuit,CurrList),
+	voltage_cons(AugCircuit,VoltList),
+	eplex_solver_setup(min(0)),
+	(foreach(C,CurrList) do sum(C) $= 0),
+	(foreach(C,VoltList) do sum(C) $= 0),
+	eplex_solve(_),
+	(foreach((_,I),AugCircuit) do eplex_var_get(I,typed_solution,I)),
+	print_list(AugCircuit),
+	compute_voltages([(A,0)],AugCircuit,Voltages1,Points),
+	sort(Voltages1,Voltages).
+
+test(Circuit,Voltages):-
 	Circuit = [
 		ground(a),
-		battery(d,a,3),
-		resistor(d,a,10),
-		battery(a,b,3),
-		resistor(d,c,3),
-		resistor(c,b,5)
+		battery(a,b,10),
+		resistor(a,b,10000),
+		resistor(b,c,2000),
+		resistor(c,a,4000),
+		resistor(c,a,8000),
+		resistor(c,a,8000)
 	],
+	solve(Circuit,Voltages).
+
+/*
 	preprocess(Circuit,AugCircuit,Points),
 	current_cons(Points,AugCircuit,CurrList),
 	voltage_cons(AugCircuit,VoltList),
@@ -102,4 +118,6 @@ test(Circuit,AugCircuit,Point):-
 	(foreach(C,CurrList) do sum(C) $= 0),
 	(foreach(C,VoltList) do sum(C) $= 0),
 	eplex_solve(_),
-	print_list(AugCircuit).
+	(foreach((_,I),AugCircuit) do eplex_var_get(I,typed_solution,I)),
+	print_list(AugCircuit),
+	compute_voltages([(a,0)],AugCircuit,Voltages,Points).*/
