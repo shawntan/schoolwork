f62eac894f57327f23d27311aaaa0550672a9724
Shawn Tan <shawn@wtf.sg>
2012-03-30 05:50:12 +0800

    reading stuff. just need to format the time.

---
+++ CS2281/lab5/U096883L-lab5-part1.c
@@ -0,0 +1,52 @@
+#include <procfs.h>
+#include <stdio.h>
+#include <malloc.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <pwd.h>
+#define DEBUG 1
+
+void print_procinfo(char *filename,psinfo_t *psinf)
+{
+//	if(DEBUG) printf("%s\n",filename);
+	FILE *fd = fopen(filename,"r");
+	psinfo_t psinfo;
+
+
+	fread(&psinfo,sizeof(psinfo_t),1,fd);
+	struct passwd *user_p = getpwuid(psinfo.pr_uid);
+
+	printf("%8s %5d %16s %d\n",
+			user_p->pw_name,
+			psinfo.pr_pid,
+			psinfo.pr_fname,
+			psinfo.pr_start.tv_sec
+	);
+}
+
+int main()
+{
+	DIR *dp;
+	struct dirent *ep;
+	psinfo_t psinfo_buf;
+	dp = opendir ("/proc");
+	char buf[128];
+	if (dp != NULL)
+	{
+		while (ep = readdir (dp))
+		{
+			if(ep->d_name[0] >= '0' && ep->d_name[0] <= '9')
+			{
+				sprintf(buf,"/proc/%s/psinfo",ep->d_name);
+				print_procinfo(buf,&psinfo_buf);
+			}
+
+		}
+
+		(void) closedir (dp);
+	}
+	else
+		perror ("Couldn't open the directory");
+
+	return 0;
+}
